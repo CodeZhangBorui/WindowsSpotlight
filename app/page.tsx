fa9fffcfd92b00fa2c8db49a396203fcbbe05af2
CodeZhangBorui <zhangborui.2008@foxmail.com>
2023-11-14 20:32:01 +0800

    Publish WindowsSpotlight

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,75 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image
+} from "@nextui-org/react";
+import axios from "axios";
 
-export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
+export default async function Home() {
+  let data = await axios
+    .get(
+      "https://arc.msn.com/v3/Delivery/Placement?pid=338387&fmt=json&cdm=1&pl=zh-CN&lc=zh-CN&ctry=CN"
+    )
+    .then((res) => {
+      let resp = res.data.batchrsp.items[0].item;
+      let data = JSON.parse(resp);
+      return data;
+    });
 
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
+  const downloadImage = () => {
+    const imageUrl = data['ad']['image_fullscreen_001_landscape']['u'];
 
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
-		</section>
-	);
+    fetch(imageUrl)
+      .then((res) => res.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", `${data['ad']['title_text']['tx']}.jpg`);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      })
+      .catch((error) => {
+        console.error("Error downloading image:", error);
+      });
+  };
+  
+  return (
+    <div className="relative h-screen flex items-center justify-center">
+      <Image
+        className="object-cover w-full h-full absolute inset-0"
+        src={data['ad']['image_fullscreen_001_landscape']['u']}
+      />
+      <Card className="max-w-[400px] absolute bottom-4 right-4">
+        <CardHeader className="flex gap-3">
+          <div className="flex flex-col">
+            <p className="text-md">{data['ad']['title_text']['tx']}</p>
+            <p className="text-small text-default-500">{data['ad']['copyright_text']['tx']}</p>
+          </div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <p>{data['ad']['hs1_title_text']['tx']}</p>
+          <p className="text-small text-default-500">{data['ad']['hs1_cta_text']['tx']}</p>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <Link
+            isExternal
+            showAnchorIcon
+            href={data['ad']['hs2_destination_url']['u'].replace('microsoft-edge:', '')}
+          >
+            探索更多
+          </Link>
+          
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
